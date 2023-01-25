5d5a0028eab421d40b48d60e71401873a661f0fb
Deeshan M <73753908+dmahida2002@users.noreply.github.com>
2023-01-25 01:34:23 -0500

    Add files via upload

---
+++ src/IP.java
@@ -0,0 +1,20 @@
+
+public class IP {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 *  + Implement JFrame or better GUi
+		 *  + Add IP Checker for VPn, bot, and give it a score (from website).
+		 * 	+ Organize data provided from checker
+		 */
+		
+		String IP = "8.8.8.8";
+		
+		Command askTo = new Command(IP);
+		
+		askTo.checkIP();
+		
+		System.out.println("\n" + askTo.getIPDetails());
+    }
+}
